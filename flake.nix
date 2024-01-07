f0d7b5c1cd141d854bb93ecc28bcf1bb5c11b85f
James Benze <james@benze.me>
2024-01-07 16:48:08 -0500

    Add development server script

--- flake.nix
+++ flake.nix
@@ -3,25 +3,44 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+    unstable.url = "nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, rust-overlay }: 
+  outputs = { self, nixpkgs, unstable, rust-overlay }: 
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
       overlays = [ rust-overlay.overlays.default ];
     };
+    myrust = pkgs.rust-bin.stable.latest.default.override {
+      targets = [ "wasm32-unknown-unknown" ];
+    };
+
+    grpcuiScript = (pkgs.writeShellScriptBin ",grpcui" "${pkgs.grpcui}/bin/grpcui -plaintext localhost:50051");
   in
   {
     devShell.${system} = pkgs.mkShell {
       packages = with pkgs; [
-        rust-bin.stable.latest.default 
+        myrust 
         protobuf_23 
         sqlx-cli 
-	(pkgs.writeShellScriptBin ",grpcui" "${grpcui}/bin/grpcui -plaintext localhost:50051")
 	sqlitebrowser
+	unstable.legacyPackages.${system}.dioxus-cli
+	grpcuiScript
+	entr
+
+	(pkgs.writeShellScriptBin ",devserver" ''
+	  set -e
+
+	  ROOT=$(${pkgs.git}/bin/git rev-parse --show-toplevel)
+
+	  ${pkgs.tmux}/bin/tmux \
+	    new-session "cd $ROOT/odr-admin; ${unstable.legacyPackages.${system}.dioxus-cli}/bin/dx serve; read" \; \
+	    split-window "cd $ROOT/odr-server; find src/ | ${pkgs.entr}/bin/entr -r ${myrust}/bin/cargo run; read" \; \
+	    select-layout even-vertical
+	'')
       ];
       PROTOC = "${pkgs.protobuf_23}/bin/protoc";
       PROTOC_INCLUDE = "${pkgs.protobuf_23}/include";

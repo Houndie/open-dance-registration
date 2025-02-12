10aa5730cfc516da569ca1fd07e8d802ab749c90
James Benze <james@benze.me>
2025-02-12 06:54:16 -0500

    Remove odr-cmd
    Instead, just put a second binary on the main server package

--- odr-server/src/bin/odr-server.rs
+++ odr-server/src/bin/odr-server.rs
@@ -1,24 +1,14 @@
-#[cfg(feature = "server")]
-pub mod server;
-
-#[cfg(feature = "server")]
-pub mod api;
-
-pub mod hooks;
-pub mod server_functions;
-pub mod view;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(feature = "web")]
     // Hydrate the application on the client
-    dioxus::launch(view::app::App);
+    dioxus::launch(odr_server::view::app::App);
 
     #[cfg(feature = "server")]
     {
         tokio::runtime::Runtime::new()
             .unwrap()
             .block_on(async move {
-                server::run_server().await.unwrap();
+                odr_server::server::run_server().await.unwrap();
             });
     }
     Ok(())
---
+++ odr-server/src/lib.rs
@@ -0,0 +1,9 @@
+#[cfg(feature = "server")]
+pub mod server;
+
+#[cfg(feature = "server")]
+pub mod api;
+
+pub mod hooks;
+pub mod server_functions;
+pub mod view;
